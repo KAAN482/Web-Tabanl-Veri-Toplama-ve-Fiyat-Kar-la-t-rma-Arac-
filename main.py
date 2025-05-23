cffae5e6a563c031c1e2f80c1d4ea6383d173f50
Kaan Sırmagül <79507714+KAAN482@users.noreply.github.com>
2025-05-23 18:34:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+1. Hepsiburada'dan dizüstü bilgisayar ürünleri çekilir.
+2. Ürün adı, fiyat, satıcı adı, puan ve kargo bilgileri ayrıştırılır.
+3. pandas ile DataFrame'e alınır.
+4. `bilgisayarlar.csv` dosyasına yazılır.
+5. Veriler sıralanarak en ucuz ve en pahalı 5 bilgisayar belirlenir.
